6d85af61809f9229c655ec1fce4b4678f989b60d
Rout <drout@deepaks-MacBook-Air.local>
2018-12-23 00:08:01 -0500

    Added some functions

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,9 +1,17 @@
 import boto3
+import click
 
-if __name__ == '__main__' :
 
-    session = boto3.Session(profile_name ='boto3practice')
-    ec2 = session.resource('ec2')
+@click.command()
+def list_instances() :
+    """List the EC2 instances"""
 
     for instance in ec2.instances.all():
-        print(instance)
+        print(f"""{instance.id},{instance.instance_type},{instance.placement['AvailabilityZone']},{instance.state['Name']},{instance.public_dns_name or 'NO VALUE'}""")
+
+session = boto3.Session(profile_name ='boto3practice')
+ec2 = session.resource('ec2')
+
+
+if __name__ == '__main__' :
+    list_instances()
