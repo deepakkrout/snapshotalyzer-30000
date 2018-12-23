9638a4e04e24a73f68015fb3d87eaad59cb7b09e
Dee Rout <deeprout.k@gmail.com>
2018-12-23 14:01:55 -0500

    Wait for instance start and stop while taking snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -12,9 +12,41 @@ def filter_instances(project) :
                 instances = list(ec2.instances.all())
         return instances
 
+@click.group('cli')
+def cli():
+    """Shotty mangement CLI script"""
+
+@cli.group('volumes')
+def volumes():
+    """Command for Volumes"""
+
+@volumes.command('list')
+@click.option('--project',default=None,help="Only volumes for Projects will be listed <tag Project : <Name>")
+def list_volumes(project):
+    instances = filter_instances(project)
+    for instance in instances :
+        for volume in instance.volumes.all():
+            print(f"""{volume.id} , {instance.id} , {volume.state}, {volume.size}GiB, {volume.encrypted and "Encrypted" or "Not Encrypted"} """)
+
+@cli.group('snapshots')
+def snapshots():
+    """Command for Snapshots"""
+
+@snapshots.command('list')
+@click.option('--project',default=None,help="Only snapshots for Projects will be listed <tag Project : <Name>")
+def list_snapshots(project):
+    instances = filter_instances(project)
+    for instance in instances :
+        for volume in instance.volumes.all():
+            for snapshot in volume.snapshots.all():
+                print(f"""{snapshot.id},{volume.id},{instance.id},{snapshot.state},{snapshot.progress},{snapshot.start_time.strftime("%c")}""")
+    return
 
 
-@click.group()
+
+
+
+@cli.group('instances')
 def instances ():
     """Command for instances"""
 @instances.command('list')
@@ -33,15 +65,15 @@ def list_instances(project) :
 
 @instances.command('stop')
 @click.option('--project',default=None,help="Only instances for Projects will be stopped <tag Project : <Name>")
+
 def stop_instances(project) :
     """Stopping EC2 instances"""
 
     instances = filter_instances(project)
-
-    for i in instances :
-        print(f"Stopping {i.id}")
-        i.stop()
-        return
+    for instance in instances:
+        print(f"Stopping {instance.id}")
+        instance.stop()
+    return
 
 
 
@@ -52,10 +84,34 @@ def start_instances(project) :
 
     instances = filter_instances(project)
 
-    for i in instances :
-        print(f"Starting {i.id}")
-        i.start()
-        return
+    instances = filter_instances(project)
+    for instance in instances :
+        print(f"Starting {instance.id}")
+        instance.start()
+    return
+
+
+
+@instances.command('snapshot')
+@click.option('--project',default=None,help="Creates snapshots of all the volumes attached to instances for Projects  <tag Project : <Name>")
+
+def create_snapshots(project):
+    """Create Snapshot of Instances"""
+    instances = filter_instances(project)
+    for instance in instances:
+        print(f"Stopping {instance.id} ...")
+        instance.stop()
+        instance.wait_until_stopped()
+        for volume in instance.volumes.all():
+            print(f"""Creating snapshot for {volume.id}...""")
+            volume.create_snapshot(Description="Created by SnapshotAlyzer 30000")
+        print(f"Starting {instance.id} ...")
+        instance.start()
+        instance.wait_until_running()
+    print("Job done")
+    return
+
+
 
 
 session = boto3.Session(profile_name ='boto3practice')
@@ -67,4 +123,4 @@ ec2 = session.resource('ec2')
 
 
 if __name__ == '__main__' :
-    instances()
+    cli()
